c9aee7d2bbf51cb4d8d20d6618874ec649d266ff
Mark McKnight <mark.mcknight@yale.edu>
2022-03-27 12:52:29 -0400

    Added, currently unused, ASCII switch component

---
+++ src/components/NHSwitch.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="nh-button" :class="{disabled}" :title="hint" @click="clickSwitch">
+    <div>┌─{{switchEdge}}─┐</div>
+    <div class="nh-click">│ {{offLabel}} │ {{onLabel}} │</div>
+    <div>└─{{switchEdge}}─┘</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NHSwitch',
+
+  props: ['onLabel', 'offLabel', 'isOn'],
+
+    data: () => ({
+    }),
+
+    computed: {
+      switchEdge () {
+        let length = this.offLabel.length + this.onLabel.length + 3
+        let returnText = ''
+        for (let i = 0; i < length; i++) {
+          returnText += '─'
+        }
+        return returnText
+      }
+    },
+
+    methods: {
+      clickButton: function (evt) {
+        if (this.disabled) return
+        this.$emit('clicked')
+        evt.preventDefault()
+      }
+    }
+  }
+</script>
+
+<style>
+.nh-click {
+  cursor: pointer;
+}
+.nh-button {
+  display: inline-block;
+  line-height: 1em;
+  font-family: "Courier New", "Menlo", "DejaVu Sans Mono", "Courier", monospace;
+  font-weight: bold;
+  user-select: none;
+  -moz-user-select: none;
+  -webkit-user-select: none;
+}
+.disabled {
+  color: #666;
+}
+</style>
