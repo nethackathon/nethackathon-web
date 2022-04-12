da41a6ddd43f84f2d73b79b9023c4dc3b38827f3
Mark McKnight <mark.mcknight@yale.edu>
2022-04-11 21:15:02 -0400

    Forgot to add Livelog component.

---
+++ src/components/Livelog.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <h1>Livelog</h1>
+    <div class="nh-textarea">
+      <v-textarea outlined v-model="allLivelog" readonly cols="80" rows="49"/>
+    </div>
+  </div>
+</template>
+
+<script>
+
+  export default {
+    name: 'Livelog',
+
+    components: {
+    },
+
+    props: ['livelog'],
+
+    computed: {
+      allLivelog: function() {
+        return [...this.livelog]
+            .sort((l, m) => parseInt(m.time) - parseInt(l.time) )
+            .map((l) => l.message)
+            .join('\n');
+      }
+    },
+
+    methods: {
+    },
+  }
+</script>
+
+<style>
+</style>
