9cacf2061ea3258eb9fe024d7c4652f487981db4
Mark McKnight <mark.mcknight@yale.edu>
2022-04-27 14:53:38 -0400

    Added winners to trophies

--- src/components/Trophies.vue
+++ src/components/Trophies.vue
@@ -7,13 +7,13 @@
     </v-row>
     <v-row>
       <v-col>
-<Trophy :before="'\n\n\n'" :after="'Fastest speed run'"/>
+<Trophy :before="'\n\n\n'" :after="'Fastest ascension\nluxidream (1:10:37)'"/>
       </v-col>
       <v-col>
-<Trophy :after="' Best egg hunter'"/>
+<Trophy :after="' Best egg hunter\n   Krump_O (7)'"/>
       </v-col>
       <v-col>
-<Trophy :before="'\n\n\n'" :after="'    ?????????'"/>
+<Trophy :before="'\n\n\n'" :after="' Best deaths (tie)\nHakurei_Deity (bisected) \n lidlraccoon (choked)'"/>
       </v-col>
     </v-row>
   </v-container>
